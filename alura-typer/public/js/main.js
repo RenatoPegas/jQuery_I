c6ad78331e347593545d7588e3fc7cb6216f0cfb
Saito11 <renatopegas9211@gmail.com>
2019-04-17 17:45:22 -0300

    inputs

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,7 +1,21 @@
-var word = $('.sentence').text();  //$ = jQuery;
-var wordNumber = word.split(" ").length; //split function separate the string by parameter inserted
-var wordSize = $('#size-number'); //alloc the length
+var word = $('.sentence').text();           //$ = jQuery;
+var wordNumber = word.split(/\S+/).length;    //split function separate the string by parameter inserted
+var wordSize = $('#size-number');           //alloc the length
 
-wordSize.text(wordNumber); //show length of sentence
+wordSize.text(wordNumber);                  //show length of sentence
 
-//  console.log(wordSize);
+var field = $('.character-field');
+
+field.on('input', function(){
+
+   var content = field.val();                //val = value in jQuery
+
+   var characters = content.length;
+   $('#character-count').text(characters);
+
+   var words = content.split(/\S+/).length -1; // /\S+/ = RegEx
+   $('#word-count').text(words);
+   
+});
+
+// console.log();
