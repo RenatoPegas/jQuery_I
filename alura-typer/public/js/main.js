875b18edc191a1fc8cc02cc3b0b2e989d2745435
Saito11 <renatopegas9211@gmail.com>
2019-04-25 08:28:48 -0300

    init of css and css manipulation with jQuery

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,43 +1,45 @@
-var initialTime = $('#game-count').text();     //initial time
+var field = $('.character-field');
+var initialTime = $('#game-count').text();           //initial time
 
-$(document).ready(function(){
+$(document).ready(function(){                        //execute when a page will be loaded
    updateWordSize();
    initializeCounts();
    initializeCronometer();
+   initializeMark();
    $('#btn-restart').click(restartGame);
 });
 
 function updateWordSize() {
 
-   var word = $('.sentence').text();              //$ = jQuery;
-   var wordNumber = word.split(/\S+/).length;     //split function separate the string by parameter inserted
-   var wordSize = $('#size-number');              //alloc the length
-   wordSize.text(wordNumber);                     //show length of sentence
+   var word = $('.sentence').text();                //$ = jQuery;
+   var wordNumber = word.split(/\S+/).length;       //split function separate the string by parameter inserted
+   var wordSize = $('#size-number');                //alloc the length
+   wordSize.text(wordNumber);                       //show length of sentence
 
 }
-
-var field = $('.character-field');
 
 function initializeCounts() {
 
    field.on('input', function () {                  //function of character and word count
 
-      var content = field.val();                  //val = value in jQuery
+      var content = field.val();                    //val = value in jQuery
 
       var characters = content.length;
       $('#character-count').text(characters);
 
-      var words = content.split(/\S+/).length - 1; // /\S+/ = RegEx
+      var words = content.split(/\S+/).length - 1;  // /\S+/ = RegEx
       $('#word-count').text(words);
 
    });
 }
 
-function initializeCronometer() {
+function initializeCronometer() {                   //function of game count
 
    var gameTime = $('#game-count').text();
 
-   field.one('focus', function () {                  //function of game count
+   field.one('focus', function () {                 
+
+      $("#btn-restart").attr("disabled", true);     //disable restart button
 
       var chonometerID = setInterval(function () {
          gameTime--;
@@ -45,21 +47,44 @@ function initializeCronometer() {
          if (gameTime < 1) {
             field.attr('disabled', true);
             clearInterval(chonometerID);
+            field.addClass('character-field-disable');
+            $("#btn-restart").attr("disabled", false); //able chronometer
          }
       }, 1000);
 
    });
 }
+function initializeMark(){
 
-// $('#btn-restart').on('click', function(){ });
+   var sentence = $('.sentence').text();
 
+   field.on('input', function(){
+
+      var textValue = field.val();
+      var compare = sentence.substr(0, textValue.length);     //compare with a part of sentence 
+      
+      if(textValue == compare){
+         field.addClass('correct-border');
+         field.removeClass('error-border');
+      }else{
+         field.addClass('error-border');
+         field.removeClass('correct-border');
+      }
+
+   });
+}
 
 function restartGame() {
    field.attr('disabled', false);
    field.val("");
    $('#word-count').text('0');
    $('#character-count').text('0');
+   
+   $('#game-count').text(initialTime);                //reinicialize count
    initializeCronometer();
+   field.removeClass('character-field-disable');
+   field.removeClass('correct-border');
+   field.removeClass('error-border');
 }
 
 
